b67f0a1e747bc355dc9d09f86fbbbe232b2813ff
RobertoRianna <rianna.roberto2000@gmail.com>
2024-06-20 17:59:45 +0200

    Add files via upload

---
+++ corso python/esercizio_range2parti.py
@@ -0,0 +1,45 @@
+
+#parte 1.base
+
+controllo= True
+while controllo:
+    numero=input("Vuoi inserire un numero? (sì/no)")
+    if numero== "sì":
+        numero1=int(input("Inserisci un numero: "))
+        if numero1 % 2 == 0:
+            print("Il numero è pari")
+        else:
+            print("Il numero è dispari")
+
+    elif numero == "no":
+        stringa=input("Inserisci una stringa: ")
+        if(len(stringa)%2 == 0):
+            print("La lunghezza della stringa è pari")
+        else:
+            print("La lunghezza della stringa è dispari")
+
+#manca la chiusura del ciclo
+
+#parte 2
+
+controllo= True
+while controllo:
+    numero=input("Vuoi inserire un numero? (sì/no)")
+    numero1=int(input("Inserisci il primo numero: "))
+    numero2=int(input("Inserisci il secondo numero: "))
+
+    if numero== "sì":
+        
+        
+        for i in range (numero1,numero2+1):
+            if i%2 ==0:
+                print(i)
+    elif numero== "no":
+        for i in range(numero1, numero2+1):
+            print(i)
+    else:
+        break
+
+
+
+
