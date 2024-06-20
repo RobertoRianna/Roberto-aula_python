ec631ed5beb0c06ed46e5529130b0d9576794793
RobertoRianna <rianna.roberto2000@gmail.com>
2024-06-20 15:17:32 +0200

    Add files via upload

--- corso python/esercizio2.py
+++ corso python/esercizio2.py
@@ -1,11 +1,14 @@
 #esercizio su Cicli e Condizioni
 #Punto 1
+controllo= True 
 
-numero=int(input("Inserisci un numero: "))
-if numero % 2 == 0:                         #verifica se il numero è pari
-    print("Il numero è pari")    
-else:
-    print("Il numero è dispari")
+while controllo:
+    numero=int(input("Inserisci un numero: "))
+    if numero % 2 == 0:                         #verifica se il numero è pari
+        print("Il numero è pari")    
+    else:
+        print("Il numero è dispari")
+        controllo=False 
 
 
 #Punto 2: Utilizzo di while e range
@@ -14,13 +17,27 @@ controllo= True    #istruzione di controllo
 
 while controllo:    
        numero1=int(input("Inserisci un numero intero positivo: "))
-       
-       for i in range(numero1,-1,-1):      #stampa dal numero inserito dall'utente a zero(compreso)
+       if numero1 > 0:
+            for i in range(numero1,-1,-1):      #stampa dal numero inserito dall'utente a zero(compreso)
               print(i)
-              numero1-1                     
+              numero1-1
+ #??????   
+#non mi fa aggiungere Else o Elif, neache break. se inserisco controllo = False non mi ferma il loop
+            
+        
+         
+         
 
 
 
 #Punto 3
+
+lista_numeri=[]
+for numero in lista_numeri:
+      
+    numero=int(input("Inserisci un numero: "))
+    print(numero * numero)
+    lista_numeri.append(numero)
+
 
 
