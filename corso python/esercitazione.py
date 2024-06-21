c6e416cfbd9228c836f0f9f0282e13c27e145365
RobertoRianna <rianna.roberto2000@gmail.com>
2024-06-21 14:49:15 +0200

    Add files via upload

--- corso python/esercitazione.py
+++ corso python/esercitazione.py
@@ -5,7 +5,30 @@ while controllo:
 
     numero=int(input("Inserisci un numero intero: "))
     if numero == 0:
-        somma= somma + numero
+        somma=somma + numero
         print(somma)
+        break
+
+
+
+#ciclo:for
+
+
+parola=input("Inserisci una parola: ")
+
+for i in parola:
+    print(parola)
+    
+
+#ciclo: range
+
+
+        
+    numero1=int(input("Inserisci il numero fino a dove vuoi contare: "))               
+    
+    for i in range (0,numero1+1,2):
+        print(i)
+
+
         
 
