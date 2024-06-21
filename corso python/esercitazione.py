18f321dfd9ffa060bd1a3387c7f476de3189648f
RobertoRianna <rianna.roberto2000@gmail.com>
2024-06-21 14:19:27 +0200

    Add files via upload

---
+++ corso python/esercitazione.py
@@ -0,0 +1,11 @@
+#ciclo: while
+controllo = True
+somma=0
+while controllo:
+
+    numero=int(input("Inserisci un numero intero: "))
+    if numero == 0:
+        somma= somma + numero
+        print(somma)
+        
+
