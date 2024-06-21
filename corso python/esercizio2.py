4c025108fd58ac1a49ac170344c49beda43bbbfa
RobertoRianna <rianna.roberto2000@gmail.com>
2024-06-21 11:09:15 +0200

    Add files via upload

--- corso python/esercizio2.py
+++ corso python/esercizio2.py
@@ -1,55 +1,22 @@
-#esercizio su Cicli e Condizioni
-#Punto 1
-controllo= True 
+#Chieda all'utente di inserire un numero intero positivo n.
+#Se il numero inserito non è positivo, chieda nuovamente l'inserimento fino a quando non viene inserito un numero positivo.
+#Una volta ottenuto un numero positivo n, il programma dovrà:
+#Stampare tutti i numeri pari da 0 a n inclusi.
+#Calcolare e stampare la somma di tutti i numeri dispari da 0 a n inclusi.
+#terminare solo dopo tre tentativi ("hai finito i tentativi") o dopo che una soma totale supera 250 ("hai vinto")
 
+controllo= True
 while controllo:
-    numero=int(input("Inserisci un numero: "))
-    if numero % 2 == 0:                         #verifica se il numero è pari
-        print("Il numero è pari")    
-    else:
-        print("Il numero è dispari")
-        controllo=False 
-
-
-#Punto 2: Utilizzo di while e range
-
-controllo= True    #istruzione di controllo
-
-while controllo:    
-       numero1=int(input("Inserisci un numero intero positivo: "))
-       if numero1 > 0:
-            for i in range(numero1,-1,-1):      #stampa dal numero inserito dall'utente a zero(compreso)
-              print(i)
-            if numero1<0:                       #SE IL NUMERO è MINORE DI 0 ESCE DAL CICLO
-                break
-              
-
-
-
-
-
-            
-        
-#Punto 3
-
-controllo= False     
-lista_numeri=[]
-if controllo== False:
-    
-
-    for numero in lista_numeri:                     #!!!non entra nel for!!!!!
-        cliente=(input("Vuoi inserire un numero?  "))
-        if cliente == "sì":
-            numeri=int(input("Inserisci un numero: "))
-            lista_numeri.append(numeri)
-            numeri=numeri * 2
-            print(lista_numeri)
-        else:
-            controllo == True
-        
-        
-        
-
+    numero=int(input("Inserisci un numero intero positivo?"))
+    if numero >0:
+         for i in range(0, numero):     #visualizza i numeri da 0 a n
+              if i%2 ==0:                   #controlla se il numero è pari
+                   print(i)             #stampa i numeri pari da 0 a n
+         break
+                              
+                         
+    elif numero <0:
+         print("Inserisci un numero positivo: ")
 
 
 
