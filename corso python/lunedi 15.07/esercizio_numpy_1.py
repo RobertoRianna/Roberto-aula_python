c3faf48efe90e7c2384ca016feed28a9f62057f0
RobertoRianna <rianna.roberto2000@gmail.com>
2024-07-15 16:12:57 +0200

    Add files via upload

--- corso python/lunedi 15.07/esercizio_numpy_1.py
+++ corso python/lunedi 15.07/esercizio_numpy_1.py
@@ -4,14 +4,14 @@ controllo = True
 while controllo:
     utente= input(print("Vuoi eseguire l'operazione: (sì) "))
     if utente == "sì":
-        arr = np.arange(10,50)
+        arr = np.arange(10,50)                                                      #creo un array che va da 10 a 49
         print(arr)
 
-        print("Tipo di dato: ",arr.dtype)  
+        print("Tipo di dato: ",arr.dtype)                                           #stampo il tipo di dato
 
-        arr = arr.astype('float64')
-        print("Tipo di dato aggiornato: ",arr.dtype)
+        arr = arr.astype('float64')                                                 #converto il tipo di dato in float
+        print("Tipo di dato aggiornato: ",arr.dtype)                                #stampo il nhovo tipo di dato
 
-        print("forma dell'array: ",arr.shape)   
+        print("forma dell'array: ",arr.shape)                                       #stampo la forma dell'array
     else:
         break
