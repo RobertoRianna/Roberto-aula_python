c08d8e7fcac24f388bfa0f68fb371dfd798ca560
RobertoRianna <rianna.roberto2000@gmail.com>
2024-07-15 17:32:45 +0200

    Add files via upload

---
+++ corso python/lunedi 15.07/esercizio_slicing_1.py
@@ -0,0 +1,15 @@
+#esercizio su numpy slicing
+import numpy as np
+
+array1D = np.arange(20,40)           #crea un array da 20 a 39                  
+print(array1D)
+
+print(array1D[0:10])                #stampa i primi 10 elementi di un array
+
+print(array1D[-5: ])                #stampa gli ultimi 5 elementi
+
+print(array1D[5:15])                #stampa gli elementi dall'indice 5 a 15(escluso)
+
+print(array1D[0:40:3])              #stampa gli elementi dell'array ogni 3 
+
+print(array1D)
