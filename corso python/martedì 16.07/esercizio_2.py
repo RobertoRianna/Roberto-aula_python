78e2f9a957cbb4afb82f073298da3d42f92bd38a
RobertoRianna <rianna.roberto2000@gmail.com>
2024-07-16 13:07:27 +0200

    Add files via upload

--- corso python/martedì 16.07/esercizio_2.py
+++ corso python/martedì 16.07/esercizio_2.py
@@ -1,16 +1,56 @@
+#esercizio lindspace, random, sum
 import numpy as np
 
-array = np.linspace(0,1,12)             #crea un aray di 12 numeri equidistanti da 0 a 1
-print(array)
+def crea_array():
+    array = np.linspace(0,1,12)             #crea un aray di 12 numeri equidistanti da 0 a 1
+    return array
 
-array = array.reshape(3,4)              #cambia la forma in 3x4
-print(array)
+def modifica_array(array):
+    array = array.reshape(3,4)              #cambia la forma in 3x4
+    return array
 
-random_array = np.random.rand(3,4)      #matrice 3x4 di numeri causuali da 0 a 1
-print(random_array)
+def array_random():
+    random_array = np.random.rand(3,4)      #matrice 3x4 di numeri causuali da 0 a 1
+    return random_array
 
-somma_array = np.sum(array)             #somma array
-print(somma_array)
+def array_somma(array):
+    somma_array = np.sum(array)             #somma array
+    return somma_array
 
-somma_random_array = np.sum(random_array)       #somma array random
-print(somma_random_array)
+def random_somma(random_array):
+    somma_random_array = np.sum(random_array)       #somma array random
+    return somma_random_array
+
+ 
+
+def menù():
+    controllo = True
+    while controllo:
+        print("Menù")
+        print("Crea un array (1) ")
+        print("modifica array (2) ")
+        print("Crea un array random (3) ")
+        print("Somma primo array (4) ")
+        print("somma array random (5) ")
+        print("Exit (6) ")
+        utente = input("Inserisci l'operazione da fare: ")
+        array = crea_array()
+        modificare_array = modifica_array(array)
+        random_array = array_random()
+        somma_array = array_somma(array)
+        somma_random = random_somma(random_array)
+        if utente == "1":
+            print(array)
+        elif utente == "2":
+            print(modificare_array)
+        elif utente == "3":
+            print(random_array)
+        elif utente == "4":
+            print(somma_array)
+        elif utente == "5":
+            print(somma_random)
+        else:
+            break
+
+menù()
+    
--- corso python/martedì 16.07/esercizio_3.py
+++ corso python/martedì 16.07/esercizio_3.py
@@ -1,21 +1,54 @@
 import numpy as np
 
-array = np.linspace(8,10,50)                                        #crea un aray di 50 numeri equidistanti da 8 a 10
-print("Array: ",array)
+def crea_array():
+    array = np.linspace(8,10,50)                                        #crea un aray di 50 numeri equidistanti da 8 a 10
+    return array
 
-random_array = np.random.random(50)                                 #50 numeri casuali compresi tra 0 a 1
-print("Array random: ",random_array)
+def crea_random():
+    random_array = np.random.random(50)                                 #50 numeri casuali compresi tra 0 a 1
+    return random_array
 
-somma_array = array + random_array                                  #somma dei due array elemento per elemento
-print("la somma dei due array per ogni elemento è: ",somma_array)
+def somma_array(array, random_array):
+    array_somma = array + random_array                                  #somma dei due array elemento per elemento
+    return array_somma
+
+def somma_totale(array_somma):
+    somma_array_nuovo = np.sum(array_somma)                                         #somma array nuovo
+    return somma_array_nuovo
+
+def somma_maggiore(array_somma):
+    array_maggiore5 = array_somma > 5
+    somma_array_maggiore5 = np.sum(array_maggiore5)                                 #somma elementi maggiore di 5 del nuovo array
+    return somma_array_maggiore5
 
 
-somma_array_nuovo = np.sum(somma_array)                                         #somma array nuovo
-print("La somma totale dell'array nuovo è: ",somma_array_nuovo)
+def menù():
+    controllo = True
+    while controllo:
+        print("Menù")
+        print("Crea un array (1) ")
+        print("Crea un array random (2) ")
+        print("Somma i due array (3) ")
+        print("Somma totale array nuovo (4) ")
+        print("Somma elementi maggiori di 5 (5) ")
+        print("Exit (6) ")
+        utente = input("Inserisci l'operazione da fare: ")
+        array = crea_array()
+        random_array = crea_random()
+        array_somma = somma_array(array,random_array)
+        somma_array_nuovo = somma_totale(array_somma)
+        array_maggiore5 = somma_maggiore(array_somma)
+        if utente == "1":
+            print(array)
+        elif utente == "2":
+            print(random_array)
+        elif utente == "3":
+            print(array_somma)
+        elif utente == "4":
+            print("La somma totale dell'array nuovo è: ",somma_array_nuovo)
+        elif utente == "5":
+            print("La somma totale dell'array con gli elementi maggiori di 5 è: ",array_maggiore5)
+        else:
+            break
 
-
-array_maggiore5 = somma_array > 5
-somma_array_maggiore5 = np.sum(array_maggiore5)                                 #somma elementi maggiore di 5 del nuovo array
-print("La somma totale dell'array con gli elementi maggiori di 5 è: ",somma_array_maggiore5)
-
-
+menù()
