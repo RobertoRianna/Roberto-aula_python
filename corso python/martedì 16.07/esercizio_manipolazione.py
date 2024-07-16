05fe649d7a0146e9b755de551d55c9d6d6160cc2
RobertoRianna <rianna.roberto2000@gmail.com>
2024-07-16 16:00:09 +0200

    Add files via upload

--- corso python/martedì 16.07/esercizio_manipolazione.py
+++ corso python/martedì 16.07/esercizio_manipolazione.py
@@ -3,15 +3,15 @@ import numpy as np
 
 def crea():
     matrice= np.arange(1,26)
-    reshaped_arr = matrice.reshape((5,5))
+    reshaped_arr = matrice.reshape((5,5))                                    #crea una matrice da 1 a 25, 5x5
     return reshaped_arr
 
 def colonna(reshaped_arr):
-    seconda_colonna = reshaped_arr[:,1]
+    seconda_colonna = reshaped_arr[:,1]                                      #stampa la seconda colonna
     return seconda_colonna
 
 def riga(reshaped_arr):
-    terza_riga = reshaped_arr[2,: ]
+    terza_riga = reshaped_arr[2,: ]                                         #stampa la terza riga
     return terza_riga
 
 def diagonale(reshapped_arr):
